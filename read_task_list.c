3c40211f1264f8b253d0734b7f73ea9cd7671eea
jiseobbyeon <alfred.j.byeon@gmail.com>
2023-05-31 22:28:48 +0900

    include read_task_list.c

--- read_task_list.c
+++ read_task_list.c
@@ -9,9 +9,11 @@
 typedef struct {
         char time[13]; // save year, month, day, hour with concat
         char filename[51];
+        char f_path[256];
         char contents[201];
+        char group_user[51];
         int flag;
-        int temp; //for joon
+        int temp;
     } task;
 
 task task_list[30];
@@ -29,10 +31,12 @@ int main(){
 task* read_dir(char* dir_path){
     DIR *dir;
     task *task_list = malloc(sizeof(task)*30);    
+    FILE* temp;
     struct dirent *filelist;
     struct stat info;
     int fd;
     int i=0;
+    int grouplen;
 
     if((dir = opendir(dir_path)) == NULL){ // opendir
         perror("cannot open directory");
@@ -53,13 +57,18 @@ task* read_dir(char* dir_path){
 
             if(strcpy(task_list[i].filename,filelist->d_name) == NULL){ // save filename 
                 perror("save filename error");
+                exit(1);
             }
 
-            if(read(fd, task_list[i].time, 13) == -1){ // first line -> save time
+            if(strcpy(task_list[i].f_path,filepath) == NULL){ // save filepath
+                perror("cannot save filepath");
+                exit(1);
+            }
+
+            if(read(fd, task_list[i].time, 10) == -1){ // first line -> save time
                 perror("save time error");
                 exit(1);
-            } 
-    
+            }
             
             lseek(fd, 1, SEEK_CUR);
 
@@ -68,46 +77,31 @@ task* read_dir(char* dir_path){
                 exit(1);
             }
 
-            if (task_list[i].flag == 48){ // 파일에 문자가 포함되어 있어서, flag 저장할 때 문자로 인식함 -> 아침에 수정
-                task_list[i].flag = 0;
-            } else task_list[i].flag = 1;
-
-            lseek(fd, 1, SEEK_CUR);
-
-            if(read(fd, task_list[i].contents, 200) == -1){ // third line -> save
+            if(read(fd, task_list[i].contents, 200) == -1){ // get contents first, will abort fd
                 perror("save contents error");
                 exit(1);
             }
-/*
+
             lseek(fd, 2, SEEK_CUR);
 
-            if(read(fd, &(task_list[i].temp), 1) == -1){
-                perror("save temp error");
-                exit(1);
-            }
+            temp = fdopen(fd, "r"); // to change int fd to FILE* type
 
-*/
+            fgets(task_list[i].group_user, 30, temp); // get line
             
             if(i==29){
                 printf("you can only read 30 tasks");
                 exit(1);
             }
             i++;
-        /* 안에서 테스트
+    
         printf("%d: filename: %s \n", i, task_list[i-1].filename);
-        printf("%d: flag %d \n", i, task_list[i-1].flag);
+        printf("%d: flag %c \n", i, task_list[i-1].flag);
+        printf("%d: group: %s \n", i, task_list[i-1].group_user);
         printf("%d: %s \n", i, task_list[i-1].contents);
-        */
         
         }
     }
-    /*
-    반복문 밖에서 테스트
-        printf("%d: filename: %s \n", i, task_list[i-1].filename);
-        printf("%d: flag %d \n", i, task_list[i-1].flag);
-        printf("%d: %s \n", i, task_list[i-1].contents);    
-
-    */     
+     
     closedir(dir);
     return task_list;
 }
